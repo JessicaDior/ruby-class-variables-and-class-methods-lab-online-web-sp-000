ac09faa0910e1063d59827abd4dcb978f1c41849
Jc Dior <jessicaclairdior@gmail.com>
2020-09-17 19:20:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,12 @@
 class Song
 
 attr_accessor :name, :artist, :genre
-attr_reader :count, :artists, :genres
+
 
 def initialize(name, artist, genre)
   @name = name
-  @artist = artist
-  @genre = genre
-end
-
-def count=(count)
-  @@count = count
-end
-
-def artists=(artists)
-  @@artists = artists
-end
-
-def genres=(genres)
-  @@genres = genres
+  @@artist = artist
+  @@genre = genre
 end
 
 
